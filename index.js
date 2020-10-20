7e77f07b649760690a160b8e53a6c5202c45b153
Artem Nehoda <47476972+Artem27-93@users.noreply.github.com>
2020-10-21 00:09:13 +0300

    remove user ability from DB

--- index.js
+++ index.js
@@ -1,54 +1,72 @@
 
 'use strict';
-    const message = {  //объект с сообщениями об отправке
-        loading: 'Загрузка',
-        success: 'Спасибо! Скоро мы с вами свяжемся.',
-        failure: 'Что-то пошло не так!',
-    }
-    const form = document.getElementById('form'),
-          db = document.querySelector('.dbase');
-   
-        form.addEventListener("submit",function(e) {
-            e.preventDefault(); // убрать стандартное поведение браузера
-        
-            const statusMessage = document.createElement('div');
-             //сообщение о статусе
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+const message = {  //объект с сообщениями об отправке
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжемся.',
+    failure: 'Что-то пошло не так!',
+}
+const form = document.getElementById('form'),
+      db = document.querySelector('.dbase');
+    
+form.addEventListener("submit",function(e) {
+    e.preventDefault(); // убрать стандартное поведение браузера
+    
+    const statusMessage = document.createElement('div');
+      //сообщение о статусе
+    statusMessage.textContent = message.loading;
+    form.append(statusMessage);
+    setTimeout(()=>{
+      statusMessage.textContent = "";
+    }, 1500);
+
+    const request = new XMLHttpRequest();
+    request.open('POST', 'test.php');
+
+    request.setRequestHeader('Content-type', 'application/json');
+    const formData = new FormData(form);
+    
+    // const json = JSON.stringify(Object.fromEntries(formData.entries()));  
+    
+    let obj = {}; // объект в который собираем все поля инпутов
+    formData.forEach((value,key)=>{
+      obj[key] = value;
+    });
+    const json = JSON.stringify(obj);
+    request.send(json); //отправка данных
+    request.addEventListener('load', ()=> {
+        if(request.status === 200){  //проверка и вывод на экран сообщения об отправке
+            // console.log(request.response);
+            db.innerHTML = request.response;// показываем на экран ответ(таблица БД)
+            statusMessage.textContent = message.success;
             setTimeout(()=>{
               statusMessage.textContent = "";
-            }, 1500);
+            }, 1500)
+        } else {
+          statusMessage.textContent = message.failure;
+          setTimeout(()=>{
+            statusMessage.textContent = "";
+          }, 1500)
+        }
+    }); 
+  });
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'test.php');
-      
-            request.setRequestHeader('Content-type', 'application/json');
-            const formData = new FormData(form);
-            
-            // const json = JSON.stringify(Object.fromEntries(formData.entries()));  
-            
-            let obj = {}; // объект в который собираем все поля инпутов
-            formData.forEach((value,key)=>{
-              obj[key] = value;
-            });
-            const json = JSON.stringify(obj);
-            request.send(json); //отправка данных
-            request.addEventListener('load', ()=> {
-                if(request.status === 200){  //проверка и вывод на экран сообщения об отправке
-                    // console.log(request.response);
-                    db.innerHTML = request.response;// показываем на экран ответ(таблица БД)
-                    statusMessage.textContent = message.success;
-                    setTimeout(()=>{
-                      statusMessage.textContent = "";
-                    }, 1500)
-                } else {
-                  statusMessage.textContent = message.failure;
-                  setTimeout(()=>{
-                    statusMessage.textContent = "";
-                  }, 1500)
-                }
-            }); 
-          });
-
-
-  
+db.addEventListener('click', (event)=>{
+  event.preventDefault();
+  let href = event.target.href; // по клику на ссылку получаю её адрес 
+  let res = href.substring(href.lastIndexOf("?")+1); // получаю из адреса ссылки 'del=...'
+// GET-запрос на удаление записи из БД
+  const xhr = new XMLHttpRequest();
+        xhr.open('GET', `test.php?${res}`, true);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+          if (this.readyState != 4) return;
+        if (this.status != 200) {
+         
+          alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+          return;
+        }else{
+          db.innerHTML = this.response;
+        }
+      }
+});
+        
--- test.php
+++ test.php
@@ -5,24 +5,23 @@
                 <th>Last_name</th>
                 <th>Patronymic</th>
                 <th>DOB</th>
+                <th>DELETE</th>
             </tr>
 <?php
 
 $postParam = file_get_contents("php://input");
 $input=json_decode($postParam,JSON_UNESCAPED_UNICODE);
-// echo (var_dump($input));
 
-
-$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-		$user = 'root'; //имя пользователя, по умолчанию это root
-		$password = ''; //пароль, по умолчанию пустой
-		$db_name = 'test'; //имя базы данных
+$host = 'localhost'; 
+		$user = 'root'; 
+		$password = ''; 
+		$db_name = 'test';
 		
 //Соединение с базой данных:
 		$link = mysqli_connect($host, $user, $password, $db_name);
 		mysqli_query($link, "SET NAMES 'utf8'");
 
-// Сохранение новой строки (до получения всех пользователей):
+// Сохранение новой строки:
 if (!empty($input)) {
 	$name = $input['userName'];
 	$surname = $input['userLastname'];
@@ -31,29 +30,35 @@ if (!empty($input)) {
     $query = "INSERT INTO test_table (Name, Surname, Patronymic, Birth) VALUES ('$name','$surname','$patronymic','$birth')";
 	mysqli_query($link, $query) or die(mysqli_error($link));
 } 
-
+// Удаление по id:
+if (isset($_GET['del'])) {
+    $del = $_GET['del'];
+    $query = "DELETE FROM test_table WHERE id=$del";
+    mysqli_query($link, $query) or die(mysqli_error($link));
+} 
 // Получение всех пользователей:
 		$query = "SELECT * FROM test_table";
 		$result = mysqli_query($link, $query) or die( mysqli_error($link) );
 		for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-        
 // Вывод на экран:
 $result = '';
-		foreach ($data as $elem) {
-			$result .= "<tr>
-                <td>$elem[id]</td>
-                <td>$elem[Name]</td>
-                <td>$elem[Surname]</td>
-                <td>$elem[Patronymic]</td>
-                <td>$elem[Birth]</td>
-            </tr>";
+    foreach ($data as $elem) {
+        $result .= "<tr>
+            <td>$elem[id]</td>
+            <td>$elem[Name]</td>
+            <td>$elem[Surname]</td>
+            <td>$elem[Patronymic]</td>
+            <td>$elem[Birth]</td>
+            <td><a href= ?del=$elem[id]>DELETE</a></td>
+        </tr>";
 	}
-	
-	echo $result;
-
-die();
+echo $result;
 ?>
  </table>
+
+
+
+
 
 
  
